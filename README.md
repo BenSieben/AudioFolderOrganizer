2aa4a47489c0ba4fda66108c4da1e21b8d2d18a8
Ben <waripod@hotmail.com>
2017-04-28 18:12:46 -0700

    Switched from eyed3 to tinytag, as tinytag supports a much larger range of audio types

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Audio Folder Organizer
 
 A simple tool which takes a folder filled with audio files, and tries to organize the files into folders by artist name and album name
 
-Created with Python 2.7 and the eyeD3 (http://eyed3.nicfit.net/) tool
+Created with Python 3.6 and the tinytag (https://pypi.python.org/pypi/tinytag/) tool
--- audio_folder_organizer.py
+++ audio_folder_organizer.py
@@ -1,52 +1,73 @@
-# MP3 Folder Organizer
-# Takes a directory filled with MP3 files as a command-line argument, and attempts to organize the files into
-#   folders based on the artist name and album name in the metadata of the MP3 files
+# Audio Folder Organizer
+# Takes a directory filled with audio files as a command-line argument, and attempts to organize the files into
+#   folders based on the artist name and album name in the metadata of the audio files
 import argparse
-import eyed3
 import os
 import re
 import shutil
+from tinytag import TinyTag
+
+# Known audio file types list
+KNOWN_AUDIO_FILE_TYPES = [
+    ".mp3",
+    ".wav",
+    ".opus",
+    ".flac",
+    ".wma",
+    ".m4a"
+]
+# Known illegal characters to have in a file name (on Windows, at least)
+FILENAME_ILLEGAL_CHARS = ["<", ">", ":", "\"", "/", "\\", "|", "?", "*"]
+for i in range(32):
+    print(str(i))
+    FILENAME_ILLEGAL_CHARS.append(chr(i))
+
+
+# Checks if the given file name should be an audio file or not, based off
+#   of the file extension the file_name has
+def is_audio_file(file_name):
+    for audio_file_type in KNOWN_AUDIO_FILE_TYPES:
+        if file_name.rfind(audio_file_type) == len(file_name) - len(audio_file_type):
+            return True
+    return False
 
 
 # Takes the given input string, and tries to filter out any characters which
 #   are illegal to have in a filename, such as "\" or """
 def filter_filename_illegal_chars(input_str):
     # Filter out single-bad characters
-    filename_illegal_chars = ["<", ">", ":", "\"", "/", "\\", "|", "?", "*"]
     filtered_str = u""
     for c in input_str:
-        if c not in filename_illegal_chars:
-            if c > 31:  # Character also cannot be in range 0 through 31 besides not being listed illegal characters
-                filtered_str = filtered_str + c
+        if c not in FILENAME_ILLEGAL_CHARS:
+            filtered_str = filtered_str + c
 
     # Filter out some of the more complex patterns
     filtered_str = re.sub("\.(\.)+", "", filtered_str)  # Get rid of multiple periods next to each other
     return filtered_str
 
 
-# Organizes MP3 files in the given directory into folders based off of metadata artist name and album name
-def organize_mp3s_in_folder(folder):
+# Organizes Audio files in the given directory into folders based off of metadata artist name and album name
+def organize_audio_files_in_folder(folder):
     # Check that given folder directory is valid (if not, quit program)
     try:
-        everything_in_folder = os.listdir(unicode(folder))  # Unicode to list items with Unicode encoding
-    except StandardError, e:
-        print str(e)
+        everything_in_folder = os.listdir(folder)
+    except BaseException as e:
+        print(str(e))
         return
 
-    # Loop through all MP3 files in the directory
+    # Loop through all audio files in the directory
     for folder_item in everything_in_folder:
         folder_item_with_path = folder + "/" + folder_item
-        # Make sure the current item is a .mp3 file
-        if os.path.isfile(folder_item_with_path) and \
-                (folder_item_with_path.rfind(".mp3") == len(folder_item_with_path) - 4):
-            # For the MP3 file, grab artist name and album name metadata
-            mp3_file = eyed3.load(folder_item_with_path)
-            mp3_file_artist = unicode(mp3_file.tag.artist)
-            mp3_file_album = unicode(mp3_file.tag.album)
+        # Make sure the current item is an audio file
+        if os.path.isfile(folder_item_with_path) and is_audio_file(folder_item):
+            # For the audio file, grab artist name and album name metadata
+            audio_tag = TinyTag.get(folder_item_with_path)
+            audio_file_artist = audio_tag.artist
+            audio_file_album = audio_tag.album
 
             # Filter out any "bad" characters from artist name / album name
-            cleaned_artist = filter_filename_illegal_chars(mp3_file_artist)
-            cleaned_album = filter_filename_illegal_chars(mp3_file_album)
+            cleaned_artist = filter_filename_illegal_chars(audio_file_artist)
+            cleaned_album = filter_filename_illegal_chars(audio_file_album)
 
             # Compute paths to artist folder / album folder
             artist_path = folder + "/" + cleaned_artist
@@ -56,23 +77,23 @@ def organize_mp3s_in_folder(folder):
             # Try to make the artist / album directories
             try:
                 os.makedirs(artist_path)
-            except OSError, e:
+            except OSError as e:
                 if not os.path.isdir(artist_path):
-                    print "WARNING: " + str(e) + "\nArtist name " + mp3_file_album + \
-                          " could not be cleaned to a valid directory name"
+                    print("WARNING: " + str(e) + "\nArtist name " + audio_file_album +
+                          " could not be cleaned to a valid directory name")
                     encountered_error = True
             try:
                 os.makedirs(album_path)
-            except OSError, e:
+            except OSError as e:
                 if not os.path.isdir(album_path):
-                    print "WARNING: " + str(e) + "\nAlbum name " + mp3_file_artist + \
-                          " could not be cleaned to a valid directory name"
+                    print("WARNING: " + str(e) + "\nAlbum name " + audio_file_artist +
+                          " could not be cleaned to a valid directory name")
                     encountered_error = True
 
-            # If there were no errors in folder creation, move MP3 to the organized folder
+            # If there were no errors in folder creation, move audio to the organized folder
             if not encountered_error:
                 destination = (album_path + "/" + folder_item)
-                print "Moving " + folder_item + " to " + (album_path + "/" + folder_item)
+                print("Moving " + folder_item + " to " + (album_path + "/" + folder_item))
                 shutil.copy2(folder_item_with_path, destination)
 
 
@@ -81,12 +102,11 @@ def organize_mp3s_in_folder(folder):
 def main():
     # Read in folder to organize
     parser = argparse.ArgumentParser()
-    parser.add_argument("mp3_folder")
+    parser.add_argument("audio_folder")
     args = parser.parse_args()
-    root_folder = args.mp3_folder
+    root_folder = args.audio_folder
 
-    # Organize the MP3 files in the given folder
-    eyed3.log.setLevel("ERROR")  # Set log level to ERROR to avoid WARNING messages from appearing in output
-    organize_mp3s_in_folder(root_folder)
+    # Organize the audio files in the given folder
+    organize_audio_files_in_folder(root_folder)
 
 main()
